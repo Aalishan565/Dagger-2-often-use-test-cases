6baf4397a7f7e96ce8c186f838bf911b419ccd0e
Aalishan Ansari <aalishan565@gmail.com>
2020-02-12 17:29:18 +0530

    -scope added

---
+++ app/src/main/java/ayesha/dagger2/dipattern/scop/ActivityScope.java
@@ -0,0 +1,10 @@
+package ayesha.dagger2.dipattern.scop;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface ActivityScope {}
